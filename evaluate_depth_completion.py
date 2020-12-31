[feature] Default evaluate without scaling. & [bug fixed] Fix mkdir error with distributed training*

evaluate_depth_completion.py
@@ -231,50 +231,47 @@ def evaluate(opt):
         # FIXME: using depth as disp now
         # pred_depth = 1 / pred_disp
         pred_depth = pred_disp
-
         mask = gt_depth > 0
 
         pred_depth *= opt.pred_depth_scale_factor
-        if not opt.disable_median_scaling:
-            # ratio = np.median(gt_depth) / np.median(pred_depth)
-            # ratio = np.mean(gt_depth) / np.mean(pred_depth)
+        # Calc ratios
+        # ratio = np.median(gt_depth) / np.median(pred_depth)
+        # ratio = np.mean(gt_depth) / np.mean(pred_depth)
+        ratio = np.mean(velodyne_depth[velodyne_mask] /
+                        pred_depth[velodyne_mask])
+        ratio_image = velodyne_depth[velodyne_mask] / pred_depth[velodyne_mask]
+        ratio = ratio_image.mean()
+        # print('ratio std {}: {}'.format(ratio_image.mean(), np.std(ratio_image)))
+        stds.append(np.std(ratio_image))
 
-            ratio = np.mean(velodyne_depth[velodyne_mask] /
-                            pred_depth[velodyne_mask])
-            # ratio = 1
-            ratio_image = velodyne_depth[velodyne_mask] / pred_depth[
-                velodyne_mask]
-            ratio = ratio_image.mean()
-            # print('ratio std {}: {}'.format(ratio_image.mean(), np.std(ratio_image)))
-            stds.append(np.std(ratio_image))
+        # vis_var = np.zeros(pred_depth.shape)
+        # var_image = np.log(abs(ratio_image - ratio))
+        # vis_var[mask_idx] = var_image
+        # max_var = vis_var.max()
+        # # Create color bar
+        # colorbar = np.uint8(
+        #     np.repeat(np.linspace(255, 0,
+        #                           num=pred_depth.shape[0])[:, np.newaxis],
+        #               100,
+        #               axis=1)[:, :, np.newaxis])
+        # colorbar = cv2.applyColorMap(colorbar, cv2.COLORMAP_JET)
+        # colorbar = cv2.putText(colorbar, '{:.2E}'.format(vis_var.max()),
+        #                        (0, 10), cv2.FONT_HERSHEY_COMPLEX, .5,
+        #                        (204, 255, 102), 1)
+        # # visualize variance
+        # vis_var /= max_var
+        # vis_var = np.uint8(vis_var * 255)
+        # vis_var = cv2.applyColorMap(vis_var, cv2.COLORMAP_JET)
+        # vis_var[:,:,0] *= velodyne_mask
+        # vis_var[:,:,1] *= velodyne_mask
+        # vis_var[:,:,2] *= velodyne_mask
+        # vis_var = np.concatenate((vis_var, colorbar), axis=1)
+        # cv2.imwrite('log_var.jpg', vis_var)
+        # time.sleep(.1)
+        # exit()
 
-            # vis_var = np.zeros(pred_depth.shape)
-            # var_image = np.log(abs(ratio_image - ratio))
-            # vis_var[mask_idx] = var_image
-            # max_var = vis_var.max()
-            # # Create color bar
-            # colorbar = np.uint8(
-            #     np.repeat(np.linspace(255, 0,
-            #                           num=pred_depth.shape[0])[:, np.newaxis],
-            #               100,
-            #               axis=1)[:, :, np.newaxis])
-            # colorbar = cv2.applyColorMap(colorbar, cv2.COLORMAP_JET)
-            # colorbar = cv2.putText(colorbar, '{:.2E}'.format(vis_var.max()),
-            #                        (0, 10), cv2.FONT_HERSHEY_COMPLEX, .5,
-            #                        (204, 255, 102), 1)
-            # # visualize variance
-            # vis_var /= max_var
-            # vis_var = np.uint8(vis_var * 255)
-            # vis_var = cv2.applyColorMap(vis_var, cv2.COLORMAP_JET)
-            # vis_var[:,:,0] *= velodyne_mask
-            # vis_var[:,:,1] *= velodyne_mask
-            # vis_var[:,:,2] *= velodyne_mask
-            # vis_var = np.concatenate((vis_var, colorbar), axis=1)
-            # cv2.imwrite('log_var.jpg', vis_var)
-            # time.sleep(.1)
-            # exit()
-
-            ratios.append(ratio)
+        ratios.append(ratio)
+        if opt.median_scaling:
             pred_depth *= ratio
 
         pred_depth = pred_depth[mask]
@@ -289,12 +286,11 @@ def evaluate(opt):
     print('std in single image | max: {:.3f}, min: {:.3f}'.format(
         np.max(stds), np.min(stds)))
 
-    if not opt.disable_median_scaling:
-        ratios = np.array(ratios)
-        med = np.median(ratios)
-        print(
-            " Scaling ratios | min: {:.3f} | max: {:.3f} | med: {:0.3f} | std: {:0.3f}"
-            .format(ratios.min(), ratios.max(), med, np.std(ratios)))
+    ratios = np.array(ratios)
+    med = np.median(ratios)
+    print(
+        " Scaling ratios | min: {:.3f} | max: {:.3f} | med: {:0.3f} | std: {:0.3f}"
+        .format(ratios.min(), ratios.max(), med, np.std(ratios)))
 
     mean_errors = np.array(errors).mean(0)
 

networks/depth_decoder.py
@@ -15,7 +15,15 @@ from layers import *
 
 
 class DepthDecoder(nn.Module):
-    def __init__(self, num_ch_enc, scales=range(4), num_output_channels=1, use_skips=True):
+    """
+    Decoder part of depth-net for depth completion
+    """
+
+    def __init__(self,
+                 num_ch_enc,
+                 scales=range(4),
+                 num_output_channels=1,
+                 use_skips=True):
         super(DepthDecoder, self).__init__()
 
         self.num_output_channels = num_output_channels
@@ -30,7 +38,8 @@ class DepthDecoder(nn.Module):
         self.convs = OrderedDict()
         for i in range(4, -1, -1):
             # upconv_0
-            num_ch_in = self.num_ch_enc[-1] if i == 4 else self.num_ch_dec[i + 1]
+            num_ch_in = self.num_ch_enc[-1] if i == 4 else self.num_ch_dec[i +
+                                                                           1]
             num_ch_out = self.num_ch_dec[i]
             self.convs[("upconv", i, 0)] = ConvBlock(num_ch_in, num_ch_out)
 
@@ -42,10 +51,12 @@ class DepthDecoder(nn.Module):
             self.convs[("upconv", i, 1)] = ConvBlock(num_ch_in, num_ch_out)
 
         for s in self.scales:
-            self.convs[("dispconv", s)] = Conv3x3(self.num_ch_dec[s], self.num_output_channels)
+            self.convs[("dispconv", s)] = Conv3x3(self.num_ch_dec[s],
+                                                  self.num_output_channels)
 
         self.decoder = nn.ModuleList(list(self.convs.values()))
         self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, input_features):
         self.outputs = {}

trainer.py
@@ -18,6 +18,7 @@ from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 
 import json
+from tqdm import tqdm
 
 from utils import *
 from kitti_utils import *
@@ -27,7 +28,6 @@ import datasets
 import networks
 from lr import WarmUpLR
 from IPython import embed
-from tqdm import tqdm
 
 
 # Freeze bn https://discuss.pytorch.org/t/freeze-batchnorm-layer-lead-to-nan/8385
@@ -38,6 +38,9 @@ def set_bn_eval(m):
 
 
 class Trainer:
+    """
+    Model trainer
+    """
 
     def __init__(self, options):
         self.opt = options
@@ -73,7 +76,6 @@ class Trainer:
                              device_ids=[self.opt.local_rank],
                              output_device=self.opt.local_rank,
                              find_unused_parameters=True)
-
 
         # data
         datasets_dict = {
@@ -171,21 +173,23 @@ class Trainer:
         k = 1
         if self.distributed:
             k = torch.distributed.get_world_size()
-        if epoch >= self.opt.warmup_epochs:
-            # self.model_optimizer = optim.Adam(
-            #     filter(lambda p: p.requires_grad, self.model.parameters()),
-            #     self.opt.learning_rate * k)
-            for param in self.model_optimizer.param_groups:
-                param['lr'] = self.opt.learning_rate * k
-            self.model_lr_scheduler = optim.lr_scheduler.StepLR(
-                self.model_optimizer, self.opt.scheduler_step_size, 0.1)
-        else:
+        if epoch < self.opt.warmup_epochs:
             self.model_optimizer = optim.Adam(
                 filter(lambda p: p.requires_grad, self.model.parameters()),
                 self.opt.learning_rate)
             delta = ((k - 1) * self.opt.learning_rate) / (
                 len(self.train_loader) * self.opt.warmup_epochs)
             self.model_lr_scheduler = WarmUpLR(self.model_optimizer, 1, delta)
+        else:
+            if hasattr(self, 'model_optimizer'):
+                for param in self.model_optimizer.param_groups:
+                    param['lr'] = self.opt.learning_rate * k
+            else:
+                self.model_optimizer = optim.Adam(
+                    filter(lambda p: p.requires_grad, self.model.parameters()),
+                    self.opt.learning_rate * k)
+            self.model_lr_scheduler = optim.lr_scheduler.StepLR(
+                self.model_optimizer, self.opt.scheduler_step_size, 0.1)
 
     def set_train(self):
         """Convert all models to training mode
@@ -377,7 +381,7 @@ class Trainer:
         """Save options to disk so we know what we ran this experiment with
         """
         models_dir = os.path.join(self.log_path, "models")
-        if not os.path.exists(models_dir):
+        if not os.path.exists(models_dir) and self.opt.local_rank == 0:
             os.makedirs(models_dir)
         to_save = self.opt.__dict__.copy()
 
@@ -385,9 +389,11 @@ class Trainer:
             json.dump(to_save, f, indent=2)
 
     def save_model(self):
+        """Save model weights to disk
+        """
         save_folder = os.path.join(self.log_path, "models",
                                    "weights_{}".format(self.epoch))
-        if not os.path.exists(save_folder):
+        if not os.path.exists(save_folder) and self.opt.local_rank == 0:
             os.makedirs(save_folder)
 
         if self.distributed:
@@ -420,10 +426,14 @@ class Trainer:
             self.model_optimizer.load_state_dict(optimizer_dict)
         else:
             if self.opt.local_rank == 0:
-                print("Cannot find Adam weights so Adam is randomly initialized")
+                print(
+                    "Cannot find Adam weights so Adam is randomly initialized")
 
 
 class FullModel(nn.Module):
+    """
+    Full training model including depth & pose branch.
+    """
 
     def __init__(self, opt, device):
         super(FullModel, self).__init__()
@@ -471,7 +481,6 @@ class FullModel(nn.Module):
             elif self.opt.pose_model_type == "posecnn":
                 self.pose = networks.PoseCNN(self.num_input_frames if self.opt.
                                              pose_model_input == "all" else 2)
-            # self.pose.to(self.device)
             self.module_names.append('pose')
 
         if self.opt.predictive_mask:
@@ -651,12 +660,12 @@ class FullModel(nn.Module):
             color = inputs[("color", 0, scale)]
             target = inputs[("color", 0, source_scale)]
 
-            # # sparse loss
-            # lidar = inputs[('lidar', 0, 0)]
-            # lidar_mask = lidar > 0
-            # sparse_loss = self.compute_sparse_loss(pred_depth, lidar,
-            #                                        lidar_mask)
-            # loss += sparse_loss
+            # sparse loss
+            lidar = inputs[('lidar', 0, 0)]
+            lidar_mask = lidar > 0
+            sparse_loss = self.compute_sparse_loss(pred_depth, lidar,
+                                                   lidar_mask)
+            loss += sparse_loss
 
             for frame_id in self.opt.frame_ids[1:]:
                 pred = outputs[("color", frame_id, scale)]
@@ -852,7 +861,8 @@ class FullModel(nn.Module):
 
         for name in self.opt.models_to_load:
             if self.opt.local_rank == 0:
-                print("[{}]Loading {} weights...".format(self.opt.local_rank, name))
+                print("[{}]Loading {} weights...".format(
+                    self.opt.local_rank, name))
             path = os.path.join(self.opt.load_weights_folder,
                                 "{}.pth".format(name))
             m = getattr(self, name)