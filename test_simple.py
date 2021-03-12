468e91aa8fe4ce0b3239d89e52778ee891e1d23e
Xinxin Chen <xinxin.chen@tusimple.ai>
2021-03-12 17:08:33 +0800

    [feat] Save more result

--- test_simple.py
+++ test_simple.py
@@ -27,22 +27,24 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='Simple testing funtion for Monodepthv2 models.')
 
-    parser.add_argument('--image_path', type=str,
-                        help='path to a test image or folder of images', required=True)
-    parser.add_argument('--model_name', type=str,
+    parser.add_argument('--image_path',
+                        type=str,
+                        help='path to a test image or folder of images',
+                        required=True)
+    parser.add_argument('--model_name',
+                        type=str,
                         help='name of a pretrained model to use',
                         choices=[
-                            "mono_640x192",
-                            "stereo_640x192",
-                            "mono+stereo_640x192",
-                            "mono_no_pt_640x192",
-                            "stereo_no_pt_640x192",
-                            "mono+stereo_no_pt_640x192",
-                            "mono_1024x320",
-                            "stereo_1024x320",
-                            "mono+stereo_1024x320"])
-    parser.add_argument('--ext', type=str,
-                        help='image extension to search for in folder', default="jpg")
+                            "mono_640x192", "stereo_640x192",
+                            "mono+stereo_640x192", "mono_no_pt_640x192",
+                            "stereo_no_pt_640x192", "mono+stereo_no_pt_640x192",
+                            "mono_1024x320", "stereo_1024x320",
+                            "mono+stereo_1024x320"
+                        ])
+    parser.add_argument('--ext',
+                        type=str,
+                        help='image extension to search for in folder',
+                        default="jpg")
     parser.add_argument("--no_cuda",
                         help='if set, disables CUDA',
                         action='store_true')
@@ -75,14 +77,16 @@ def test_simple(args):
     # extract the height and width of image that this model was trained with
     feed_height = loaded_dict_enc['height']
     feed_width = loaded_dict_enc['width']
-    filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
+    filtered_dict_enc = {
+        k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()
+    }
     encoder.load_state_dict(filtered_dict_enc)
     encoder.to(device)
     encoder.eval()
 
     print("   Loading pretrained decoder")
-    depth_decoder = networks.DepthDecoder(
-        num_ch_enc=encoder.num_ch_enc, scales=range(4))
+    depth_decoder = networks.DepthDecoder(num_ch_enc=encoder.num_ch_enc,
+                                          scales=range(4))
 
     loaded_dict = torch.load(depth_decoder_path, map_location=device)
     depth_decoder.load_state_dict(loaded_dict)
@@ -95,12 +99,18 @@ def test_simple(args):
         # Only testing on a single image
         paths = [args.image_path]
         output_directory = os.path.dirname(args.image_path)
+        out_dir_npy = output_directory
+        out_dir_jpeg = output_directory
     elif os.path.isdir(args.image_path):
         # Searching folder for images
-        paths = glob.glob(os.path.join(args.image_path, '*.{}'.format(args.ext)))
-        output_directory = args.image_path
+        paths = glob.glob(os.path.join(args.image_path,
+                                       '*.{}'.format(args.ext)))
+        output_directory = args.image_path.rstrip('/').rsplit('/', 1)[0]
+        out_dir_npy = os.path.join(output_directory, 'disp')
+        out_dir_jpeg = os.path.join(output_directory, 'disp_jpeg')
     else:
-        raise Exception("Can not find args.image_path: {}".format(args.image_path))
+        raise Exception("Can not find args.image_path: {}".format(
+            args.image_path))
 
     print("-> Predicting on {:d} test images".format(len(paths)))
 
@@ -115,7 +125,8 @@ def test_simple(args):
             # Load image and preprocess
             input_image = pil.open(image_path).convert('RGB')
             original_width, original_height = input_image.size
-            input_image = input_image.resize((feed_width, feed_height), pil.LANCZOS)
+            input_image = input_image.resize((feed_width, feed_height),
+                                             pil.LANCZOS)
             input_image = transforms.ToTensor()(input_image).unsqueeze(0)
 
             # PREDICTION
@@ -125,27 +136,33 @@ def test_simple(args):
 
             disp = outputs[("disp", 0)]
             disp_resized = torch.nn.functional.interpolate(
-                disp, (original_height, original_width), mode="bilinear", align_corners=False)
+                disp, (original_height, original_width),
+                mode="bilinear",
+                align_corners=False)
 
             # Saving numpy file
             output_name = os.path.splitext(os.path.basename(image_path))[0]
-            name_dest_npy = os.path.join(output_directory, "{}_disp.npy".format(output_name))
+            name_dest_npy = os.path.join(out_dir_npy,
+                                         "{}_disp.npy".format(output_name))
             scaled_disp, _ = disp_to_depth(disp, 0.1, 100)
             np.save(name_dest_npy, scaled_disp.cpu().numpy())
 
             # Saving colormapped depth image
             disp_resized_np = disp_resized.squeeze().cpu().numpy()
             vmax = np.percentile(disp_resized_np, 95)
-            normalizer = mpl.colors.Normalize(vmin=disp_resized_np.min(), vmax=vmax)
+            normalizer = mpl.colors.Normalize(vmin=disp_resized_np.min(),
+                                              vmax=vmax)
             mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
-            colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
+            colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] *
+                              255).astype(np.uint8)
             im = pil.fromarray(colormapped_im)
 
-            name_dest_im = os.path.join(output_directory, "{}_disp.jpeg".format(output_name))
+            name_dest_im = os.path.join(out_dir_jpeg,
+                                        "{}_disp.jpeg".format(output_name))
             im.save(name_dest_im)
 
-            print("   Processed {:d} of {:d} images - saved prediction to {}".format(
-                idx + 1, len(paths), name_dest_im))
+            print("   Processed {:d} of {:d} images - saved prediction to {}".
+                  format(idx + 1, len(paths), name_dest_im))
 
     print('-> Done!')
 
